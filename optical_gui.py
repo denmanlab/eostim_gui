5edbb8145ce3ad39117998de43b7f06f0da0e72e
granthughes1999 <grant.2.hughes@cuanschutz.edu>
2021-12-17 11:09:32 -0700

    adding o_1_start and o_1_end

--- optical_gui.py
+++ optical_gui.py
@@ -29,6 +29,7 @@ notes = []
 
 
 
+
 o_duration = 0
 o_delay = 0
 pulse_number = 0
@@ -68,8 +69,12 @@ def do_train():
     parameter.append(parameter_window.get())
     notes.append(notes_window.get())
 
-    o_duration = float(o_duration_entry.get())
-    o_delay = float(o_delay_entry.get())
+#     o_duration = float(o_duration_entry.get())
+#     o_delay = float(o_delay_entry.get())
+    
+    o_1_start = float(o_1_start.get())
+    o_1_end = float(o_1_end.get())
+    
     train_number = float(run_x_times_entry.get())
     pulse_number = float(pulse_num_entry.get())
     time_between_pulses = float(time_between_pulses_entry.get())
@@ -84,8 +89,11 @@ def do_train():
         'notes': [notes[-1]]*int(train_number),
         
 
-        'o_duration': [o_duration] *int(train_number),
-        'o_delay': [o_delay]* int(train_number),
+#         'o_duration': [o_duration] *int(train_number),
+#         'o_delay': [o_delay]* int(train_number),
+        
+        
+        
         'number_of_trains': [train_number]*int(train_number),
         'pulse_number': [pulse_number]*int(train_number),
         'time_between_pulses': [time_between_pulses]* int(train_number),
@@ -171,13 +179,17 @@ notes_label = Label(app, text='Notes')
 notes_window = Entry(app, text='')
 
 ### Int_windows
-o_duration_label = Label(app, text= "Optical Pulse Duration (sec)")
-o_duration_entry = Entry(app, text="Optical Duration Seconds")
-o_duration_entry.insert(0,o_duration)
+# o_duration_label = Label(app, text= "Optical Pulse Duration (sec)")
+# o_duration_entry = Entry(app, text="Optical Duration Seconds")
+# o_duration_entry.insert(0,o_duration)
 
-o_delay_label = Label(app, text= "o_delay")
-o_delay_entry = Entry(app, text="o_delay")
-o_delay_entry.insert(0,o_delay)
+# o_delay_label = Label(app, text= "o_delay")
+# o_delay_entry = Entry(app, text="o_delay")
+# o_delay_entry.insert(0,o_delay)
+
+
+o_1_start_entry = Entry(app, text = 'o_2')
+o_1.insert(0,o_1_start)
 
 pulse_num_label = Label(app, text="(pulse_number) Number_of_pulses_per_train", fg='#9900ff')
 pulse_num_entry = Entry(app, text="")
@@ -217,23 +229,25 @@ notes_window.grid(row=7, column=0)
 
 ## int_windows
 
-o_duration_label.grid(row=1, column=1)
-o_duration_entry.grid(row=2, column=1)
+# o_duration_label.grid(row=1, column=1)
+# o_duration_entry.grid(row=2, column=1)
 
-o_delay_label.grid(row=3, column=1)
-o_delay_entry.grid(row=4, column=1)
+# o_delay_label.grid(row=3, column=1)
+# o_delay_entry.grid(row=4, column=1)
 
-run_x_times_label.grid(row=5, column=1)
-run_x_times_entry.grid(row=6, column=1)
+o_1_start.grid(row=1, column=1)
 
-pulse_num_label.grid(row=7, column=1) 
-pulse_num_entry.grid(row=8, column=1) 
+run_x_times_label.grid(row=1, column=2)
+run_x_times_entry.grid(row=2, column=2)
 
-time_between_pulses_label.grid(row=9, column=1)
-time_between_pulses_entry.grid(row=10, column=1)
+pulse_num_label.grid(row=3, column=2) 
+pulse_num_entry.grid(row=4, column=2) 
 
-time_between_trains_label.grid(row=11, column=1) 
-time_between_trains_entry.grid(row=12, column=1) 
+time_between_pulses_label.grid(row=5, column=2)
+time_between_pulses_entry.grid(row=6, column=2)
+
+time_between_trains_label.grid(row=7, column=2) 
+time_between_trains_entry.grid(row=8, column=2) 
 
 
 
